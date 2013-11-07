d9ad5b48503ca64b97c28547422d89da1339bace
Marco Ceppi <marco@ceppi.net>
2013-11-06 20:12:23 -0500

    Initial commit

---
+++ plugin.rb
@@ -0,0 +1,23 @@
+# name: discourse-ubuntu
+# about: Ubuntu SSO support for Discourse
+# version: 0.2
+# authors: Sam Saffron, Marco Ceppi
+
+auth_provider :title => 'with Ubuntu',
+              :authenticator => Auth::OpenIdAuthenticator.new('ubuntu','https://login.ubuntu.com', trusted: true),
+              :message => 'Authenticating with Ubuntu (make sure pop up blockers are not enbaled)',
+              :frame_width => 1000,   # the frame size used for the pop up window, overrides default
+              :frame_height => 800
+
+register_css <<CSS
+
+.btn-social.ubuntu {
+  background: #dd4814;
+}
+
+.btn-social.ubuntu:before {
+  font-family: Ubuntu;
+  content: "U";
+}
+
+CSS
